42331a7dee7b4f3ce908f3c2567b0431d1909873
Sergey <loveykosergey@ukr.net>
2021-11-04 00:15:32 +0200

    feat: complete task №1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,12 @@
 const fs = require('fs');
+const path = require('path');
+const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
+stream.on('readable',  () => {
+    const data = stream.read();
+    if (data) {
+        console.log(data.toString());
+    }
+});
+
+// const fileContent = fs.readFileSync('01-read-file/text.txt', 'utf8');
+// console.log(fileContent);
