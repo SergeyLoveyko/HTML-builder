aa74abe3e721c37ea1501c7913c64899fd648892
Sergey <loveykosergey@ukr.net>
2021-11-08 23:03:10 +0200

    feat: complete task №4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 
+const oldDir = path.join(__dirname, 'files');
+
+
+fs.readdir(oldDir, { withFileTypes: true }, (err, files) => {
+    if (err) {
+        throw err;
+    }
+    fs.mkdir("04-copy-directory/files-copy", { recursive: true }, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    const newDir = path.join(__dirname, 'files-copy');
+    files.forEach((el) => {
+        fs.readFile(`${oldDir}/${el.name}`, (err, data) => {
+            if (err) {
+                throw err;
+            } else {
+                fs.writeFile(`${newDir}/${el.name}`, data, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            }
+        });
+    });
+});
